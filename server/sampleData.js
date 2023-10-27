75c43b5d020e3a8d7f92d7772d507e285d9d79d5
Shubhodeep <banerjeeshubho98@gmail.com>
2023-10-27 20:24:03 +0530

    changes

---
+++ server/sampleData.js
@@ -0,0 +1,77 @@
+// Projects
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description:"This is an E-commerce website which is made specially for buying and selling authentic Ethnic outfits for boys mens and womens and childrens as well.",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Dating App",
+    description:"This is project for Dtaing apps where you can find your life partner with whom you wanna spend your life and want to make a good support sytem for the future.",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "SEO Project",
+    description:
+      "This is an SEO based project where you can make your website SEO friendly and increase your sales for maximum profits for your online business.",
+    status: "In Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Design Prototype",
+    description:
+      "This is a project for amazing designers who want to enhance their art and craft skils and increase the Designing skills for a better designing career.",
+    status: "Done",
+  },
+  {
+    id: "5",
+    clientId: "5",
+    name: "Auction Website",
+    description:
+      "This is a Auction project where the biding goes on for paintings done by the painters who have come all across the world to sell their amazing looking paintings.",
+    status: "In Progress",
+  },
+];
+
+// Clients
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "2",
+    name: "Natasha Romanova",
+    email: "blackwidow@gmail.com",
+    phone: "223-567-3322",
+  },
+  {
+    id: "3",
+    name: "Thor Odinson",
+    email: "thor@gmail.com",
+    phone: "324-331-4333",
+  },
+  {
+    id: "4",
+    name: "Steve Rogers",
+    email: "steve@gmail.com",
+    phone: "344-562-6787",
+  },
+  {
+    id: "5",
+    name: "Bruce Banner",
+    email: "bruce@gmail.com",
+    phone: "321-468-8887",
+  },
+];
+
+module.exports = { projects, clients };
